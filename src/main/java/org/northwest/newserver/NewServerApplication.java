fde49b69128b7a07e625f5e9e2e25397889a1c4f
felix <592890034@qq.com>
2022-04-07 16:22:12 +0800

    2022.4.7

--- src/main/java/org/northwest/newserver/NewServerApplication.java
+++ src/main/java/org/northwest/newserver/NewServerApplication.java
@@ -8,14 +8,22 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NewServerApplication extends SpringBootServletInitializer {
+public class NewServerApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(NewServerApplication.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(NewServerApplication.class, args);
     }
 
+}
+
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ */
+class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(NewServerApplication.class);
+    }
 }
